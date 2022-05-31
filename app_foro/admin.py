11bec2d6ef354b45b4fb218144939a59066970c4
Berni Valentinuzzi <bernii.valentinuzzi@hotmail.com>
2022-05-31 20:53:25 -0300

    Nos equivocamos cuando dijimos que el cuarto commit era el ultimo. Quinto commit con cambios en el archivo admin.py

--- app_foro/admin.py
+++ app_foro/admin.py
@@ -1,11 +1,29 @@
 from django.contrib import admin
 from app_foro.models import *
 
+
 # Register your models here.
 
 
-admin.site.register(publicacion)
-admin.site.register(contacto)
-admin.site.register(sorteo)
+class display_publicaciones(admin.ModelAdmin):
+    list_display = ('usuario'  , 'tema_especifico' , 'consulta' , 'fecha_de_publicacion')
+    list_filter = ('tema_especifico' , )
+    search_fields = ('consulta' , )
+
+
+class display_contacto(admin.ModelAdmin):
+    list_display = ('usuario' , 'email' , 'razon_contacto' , 'fecha_de_contacto')
+    search_fields = ('usuario' , )
+
+
+class display_sorteo(admin.ModelAdmin):
+    list_display = ('nombre' , 'apellido' , 'email' , 'telefono')
+    list_filter = ('nombre' , 'apellido')
+    search_fields = ('apellido' , )
+    
+
+admin.site.register(publicacion , display_publicaciones)
+admin.site.register(contacto , display_contacto)
+admin.site.register(sorteo , display_sorteo)
 
 
